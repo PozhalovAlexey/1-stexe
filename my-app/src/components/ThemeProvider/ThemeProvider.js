ea9b22bda347d14b0775526ff191bc3d03a448c5
pozhalov_AI <pozhalov7@mail.ru>
2023-03-20 19:59:01 +0300

    Начал разработку темной темы

---
+++ my-app/src/components/ThemeProvider/ThemeProvider.js
@@ -0,0 +1,57 @@
+// import React, { useContext, useState } from "react";
+//import { createTheme } from "@material-ui/core";
+
+// import(createTheme(useContext(primary, secondary)));
+// import { Header } from "../Header";
+//
+// const ThemeContext = createTheme({ type: "Light" });
+//
+// export const ThemeProvider = ({ children }) => {
+//   const [type, setType] = useState("Light");
+//   return (
+//     <ThemeContext.Provider value={{ type, setType }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
+
+const DarkMode = () => {
+  const setDarkMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+  };
+  const setLightMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  };
+
+  const toggleTheme = (e) => {
+    if (e.target.checked) setDarkMode();
+    else setLightMode();
+  };
+  return (
+    <div className={"dark__mode"}>
+      <input
+        className={"dark__mode__input"}
+        type={"checkbox"}
+        id={"darkmode__toggle"}
+        onChange={toggleTheme}
+      />
+      <label
+        className={"dark__mode__label"} /* for={"darkmode__toggle"} */
+      ></label>
+    </div>
+  );
+};
+//
+// const theme = () => {
+//   createTheme({
+//     palette: {
+//       primary: {
+//         backgroundColor: "#fff",
+//       },
+//       secondary: {
+//         backgroundColor: "#222",
+//       },
+//     },
+//   });
+// };
+// export default theme;
